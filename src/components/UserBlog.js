a3e46d00556bb4b16e1f226fff01dfc70a6d538d
Tejprakash <67041377+Tej988@users.noreply.github.com>
2022-08-07 19:43:11 +0530

    Update UserBlog.js

--- src/components/UserBlog.js
+++ src/components/UserBlog.js
@@ -8,7 +8,7 @@ const UserBlog = () => {
 //   const id = localStorage.getItem("userId");
 
 //   const sendRequest = async()=>{
-//     const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`)
+//     const res = await axios.get(`https://xenon-blog.herokuapp.com/api/blog/user/${id}`)
 //     .catch((err)=> console.log(err))
 //     const data = await res.data;
 //     return data;
@@ -19,7 +19,7 @@ const UserBlog = () => {
     const id = localStorage.getItem("userId");
     
       const sendRequest = async()=>{
-    const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`)
+    const res = await axios.get(`https://xenon-blog.herokuapp.com/api/blog/user/${id}`)
     .catch((err)=> console.log(err))
     const data = await res.data;
     return data;
